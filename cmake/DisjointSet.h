270b6b07b4ff47a2046057723c03a069866538eb
DSCANGSTF <91268690+DSCANGSTF@users.noreply.github.com>
2021-09-29 22:29:37 -0400

    Add files via upload

---
+++ cmake/DisjointSet.h
@@ -0,0 +1,26 @@
+#ifndef PPSCAN_DISJOINTSET_H
+#define PPSCAN_DISJOINTSET_H
+
+#include <vector>
+
+using ui=unsigned int;
+using namespace std;
+
+// disjoint-set related, see CLRS chapter 22 for detail
+class DisjointSet {
+    void MakeDisjointSetBatch(ui capacity);
+
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    explicit DisjointSet(ui capacity);
+
+    int FindRoot(int u);
+
+    void Union(int u, int v);
+
+    bool IsSameSet(int u, int v);
+};
+
+#endif //DSCAN_DISJOINTSET_H
---
+++ cmake/main.cpp
@@ -0,0 +1,60 @@
+#include <boost/python.hpp>
+using namespace boost::python; 
+ 
+ 
+#include <cstring>
+
+#include <iostream>
+#include <chrono>
+
+#ifdef WITHGPERFTOOLS
+
+#include <gperftools/profiler.h>
+
+#endif
+
+#include "Graph.h"
+
+void Usage() {
+    cout << "Usage: [1]exe [2]graph-dir [3]similarity-threshold [4]density-threshold [5 optional]Output\n";
+}
+
+BOOST_PYTHON_MODULE(DSCAN) 
+{ 
+
+   int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        Usage();
+    } else {
+        // input
+        using namespace std::chrono;
+        auto io_start = high_resolution_clock::now();
+        auto *graph = new Graph(argv[1], argv[2], atoi(argv[3]));
+        auto io_end = high_resolution_clock::now();
+        cout << "\nTotal input cost:" << duration_cast<milliseconds>(io_end - io_start).count() << " ms\n";
+
+        // compute
+        auto start = high_resolution_clock::now();
+#ifdef WITHGPERFTOOLS
+        cout << "with google perf start------------\n";
+        ProfilerStart("DSCANProfile.log");
+#endif
+        graph->DSCAN();
+#ifdef WITHGPERFTOOLS
+        cout << "with google perf end--------------\n";
+        ProfilerStop();
+#endif
+        auto end = high_resolution_clock::now();
+        cout << "Total time without IO:" << duration_cast<milliseconds>(end - start).count() << " ms\n";
+
+        // Output
+        io_start = high_resolution_clock::now();
+        if (argc >= 5 && strcmp(argv[4], "output") == 0) { graph->Output(argv[2], argv[3]); }
+        io_end = high_resolution_clock::now();
+        cout << "Total output cost:" << duration_cast<milliseconds>(io_end - io_start).count() << " ms\n\n";
+    }
+    return 0;
+  }
+
+} 
+
